c0518c4396f85703cafc558e8f870546b7e3aa73
sumudu-k <lakshankulathunga21@gmail.com>
2025-02-15 12:20:30 +0530

    created the counter with 'useState' hook🙌

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './testing.css';
 
 export default function Testing() {
+
+    const [count, setCount] = useState(45);
+
+    function increment() {
+        setCount(count + 1);
+    }
+
+    function decrement() {
+        setCount(count - 1);
+    }
+
+
     return (
         <div className='background'>
-            <button className='button'>+</button>
-            <span>0</span>
-            <button className='button'>-</button>
+            <button onClick={decrement}>-</button>
+            <span>{count}</span>
+            <button onClick={increment} >+</button>
         </div>
     )
 }
+
