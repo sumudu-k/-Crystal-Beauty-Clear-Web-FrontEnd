44d4adbebeb74288a420a26f7a788beef382b6fe
sumudu-k <lakshankulathunga21@gmail.com>
2025-02-17 18:20:59 +0530

    add buttons to which is selected

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -4,6 +4,7 @@ import './testing.css';
 export default function Testing() {
 
     const [count, setCount] = useState(45);
+    const [name, setName] = useState("Students");
 
     function increment() {
         setCount(count + 1);
@@ -13,13 +14,24 @@ export default function Testing() {
         setCount(count - 1);
     }
 
+    function changeName(nameValue) {
+        setName(nameValue);
+    }
 
     return (
         <div className='background'>
-            <button onClick={decrement}>-</button>
+            <button className='val' onClick={decrement}>-</button>
             <span>{count}</span>
-            <button onClick={increment} >+</button>
-        </div>
+            <button className='val' onClick={increment} >+</button>
+            <h1>{name}</h1>
+
+            <div className='button-panel'>
+                <button onClick={() => changeName("Students")} >Students</button>
+                <button onClick={() => changeName("Teachers")} >Teachers</button>
+                <button onClick={() => changeName("Principles")}>Principles</button>
+            </div>
+
+        </div >
     )
 }
 
