01f49fb925b4a649c7c51a4ef0e1e5514ab98559
sumudu-k <lakshankulathunga21@gmail.com>
2025-02-18 21:07:48 +0530

    imported react-icons and design sidebar

--- src/adminHomePage.jsx
+++ src/adminHomePage.jsx
@@ -1,13 +1,20 @@
 import react from 'react';
 import { Link } from 'react-router-dom';
+import { MdDashboardCustomize } from "react-icons/md";
+import { FaUserFriends, FaBoxOpen, FaCartPlus } from "react-icons/fa";
+
 export default function AdminHomePage() {
     return (
         <div className='bg-blue-400 w-full h-screen flex' >
-            <div className='w-[20%] h-screen bg-blue-700 flex flex-col items-center'>
-                <Link to='/admin/dashboard'>Dashboard</Link>
-                <Link to='/admin/products'>Products</Link>
-                <Link to='/admin/orders'>Orders</Link>
-                <Link to='/admin/customers'>Customers</Link>
+            <div className='w-[60%] h-screen bg-blue-700 flex flex-col items-center text-white' >
+
+                <Link to='/admin/dashboard' className='flex flex-row items-center mb-4 mt-4  hover:text-amber-300'><MdDashboardCustomize className='mr-2' /> Dashboard</Link>
+
+                <Link to='/admin/products' className='flex flex-row items-center mb-4 hover:text-amber-300'> <FaBoxOpen className='mr-2' />Products</Link>
+
+                <Link to='/admin/orders' className='flex flex-row items-center mb-4 hover:text-amber-300'><FaCartPlus className='mr-2' /> Orders</Link>
+
+                <Link to='/admin/customers' className='flex flex-row items-center mb-4 hover:text-amber-300'> <FaUserFriends className='mr-2' />Customers</Link>
             </div>
 
             <div className='w-[80%] h-screen bg-green-600'></div>
