d6a5177ca8344939584050798ce32f6a4e12c65d
sumudu-k <lakshankulathunga21@gmail.com>
2025-02-18 20:44:16 +0530

    created admin dashboard layout

--- src/adminHomePage.jsx
+++ src/adminHomePage.jsx
@@ -1,9 +1,16 @@
 import react from 'react';
-
+import { Link } from 'react-router-dom';
 export default function AdminHomePage() {
     return (
-        <div>
-            <h1>Admin Home Page</h1>
+        <div className='bg-blue-400 w-full h-screen flex' >
+            <div className='w-[20%] h-screen bg-blue-700 flex flex-col items-center'>
+                <Link to='/admin/dashboard'>Dashboard</Link>
+                <Link to='/admin/products'>Products</Link>
+                <Link to='/admin/orders'>Orders</Link>
+                <Link to='/admin/customers'>Customers</Link>
+            </div>
+
+            <div className='w-[80%] h-screen bg-green-600'></div>
         </div>
     )
 }
