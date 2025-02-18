dae80fbc7b88c35692ee804763c09f1fde967e16
sumudu-k <lakshankulathunga21@gmail.com>
2025-02-18 19:39:39 +0530

    created admin home page and set route

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import UserData from './components/UserData'
 import HomePage from './pages/homePage'
 import LoginPage from './pages/loginPage'
 import SignupPage from './pages/signupPage'
+import AdminHomePage from './adminHomePage'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes path="/*">
-          <Route path="/" element={<HomePage />} />
+          <Route path="/*" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
-          <Route path="/*" element={<h1>ERROR 404 PAGE NOT FOUND</h1>} />
+          {/* <Route path="/*" element={<h1>ERROR 404 PAGE NOT FOUND</h1>} /> */}
+          <Route path="/admin/*" element={<AdminHomePage />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/adminHomePage.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+
+export default function AdminHomePage() {
+    return (
+        <div>
+            <h1>Admin Home Page</h1>
+        </div>
+    )
+}
+
