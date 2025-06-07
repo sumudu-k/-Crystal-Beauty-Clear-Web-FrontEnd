209ed4fdbed8875dc5bbc2d5b74dd1c49ee0ec65
sumudu-k <lakshankulathunga21@gmail.com>
2025-06-07 20:17:11 +0530

    creates image e upload as a promise

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,35 +1,17 @@
 import { useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkYXVsYmhkZWhlb2NtZHV2bXRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyMTEwMTgsImV4cCI6MjA1NTc4NzAxOH0.xCFsJVMudn2CV3hFjOrQ3PdFi8-ra4pM2YNhbNJBHD4`;
-
-const url = "https://udaulbhdeheocmduvmtn.supabase.co";
+import uploadMediaToSupabase from "../utils/mediaUpload";
 
 export default function FileUploadTest() {
   const [file, setFile] = useState(null);
 
-  async function handleUpload() {
-    if (file == null) {
-      alert("Please select a file to upload");
-      return;
-    }
-    console.log(file);
-
-    let fileName = file.name;
-    const extension = fileName.split(".").pop();
-    console.log(extension);
-
-    const supabase = createClient(url, key);
-
-    const timestamp = new Date().getTime();
-    fileName = timestamp + "." + extension;
-
-    await supabase.storage.from("images").upload(fileName, file, {
-      cacheControl: "3600",
-      upsert: false,
-    });
-    const publicUrl = supabase.storage.from("images").getPublicUrl(fileName);
-    console.log(publicUrl);
+  function handleUpload() {
+    uploadMediaToSupabase(file)
+      .then((url) => {
+        console.log("File uploaded successfully:", url);
+      })
+      .catch((error) => {
+        console.error("Error uploading file:", error);
+      });
   }
   return (
     <div>
---
+++ src/utils/mediaUpload.js
@@ -0,0 +1,39 @@
+import { createClient } from "@supabase/supabase-js";
+// Supabase configuration
+const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkYXVsYmhkZWhlb2NtZHV2bXRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyMTEwMTgsImV4cCI6MjA1NTc4NzAxOH0.xCFsJVMudn2CV3hFjOrQ3PdFi8-ra4pM2YNhbNJBHD4`;
+
+const url = "https://udaulbhdeheocmduvmtn.supabase.co";
+
+
+export default function uploadMediaToSupabase(file){
+
+    return new Promise((resolve,reject)=>{
+        if(file==null){
+            reject("Please select a file to upload");
+            //return;
+        }
+        let fileName = file.name;
+    const extension = fileName.split(".").pop();
+
+    const supabase = createClient(url, key);
+
+    const timestamp = new Date().getTime();
+    fileName = timestamp + "." + extension;
+
+
+    supabase.storage.from("images").upload(fileName, file, {
+      cacheControl: "3600",
+      upsert: false,
+    }).then(()=>{
+        const publicUrl = supabase.storage.from("images").getPublicUrl(fileName);
+        console.log(publicUrl);
+        resolve(publicUrl);
+    }).catch((error)=>{
+        console.error("Error uploading file:", error);
+        reject(error);
+
+    });
+})
+}
+    
+
