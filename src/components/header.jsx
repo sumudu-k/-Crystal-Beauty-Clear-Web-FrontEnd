13400c9903973086b7c2058c92aac4fe3f47a29b
sumudu-k <lakshankulathunga21@gmail.com>
2025-06-08 16:54:24 +0530

    created header in home page

---
+++ src/components/header.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="bg-accent w-full h-[80px] relative flex  items-center justify-center">
+      <img
+        src="../public/l.jpg"
+        alt=""
+        className="h-[80%] rounded-full cursor-pointer absolute left-[20px]"
+      />
+      <div className="h-full flex  items-center w-[500px] justify-between">
+        <Link
+          to="/"
+          className="text-white font-bold  text-[20px] hover:border-b border-b-white">
+          Home
+        </Link>
+
+        <Link
+          to="/products"
+          className="text-white font-bold  text-[20px] hover:border-b border-b-white">
+          Products
+        </Link>
+
+        <Link
+          to="/about"
+          className="text-white font-bold  text-[20px] hover:border-b border-b-white">
+          About Us
+        </Link>
+
+        <Link
+          to="/contact"
+          className="text-white font-bold  text-[20px] hover:border-b border-b-white">
+          Contact Us
+        </Link>
+      </div>
+    </header>
+  );
+}
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import Header from "../components/header";
 
 export default function HomePage() {
-  return <div className="h-screen w-full">slk</div>;
+  return (
+    <div className="h-screen w-full">
+      <Header />
+      <Routes path="/*"></Routes>
+    </div>
+  );
 }
