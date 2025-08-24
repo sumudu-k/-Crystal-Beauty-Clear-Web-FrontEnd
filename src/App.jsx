1d0e17ab6241f7a9583d41cea4c7a36307cbaf14
sumudu-k <lakshankulathunga21@gmail.com>
2025-08-24 22:46:51 +0530

    setup frontend google login

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,22 @@ import LoginPage from "./pages/loginPage";
 import SignupPage from "./pages/signupPage";
 import AdminHomePage from "./pages/adminHomePage";
 import { Toaster } from "react-hot-toast";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Toaster />
-        <Routes path="/*">
-          <Route path="/*" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          {/* <Route path="/*" element={<h1>ERROR 404 PAGE NOT FOUND</h1>} /> */}
-          <Route path="/admin/*" element={<AdminHomePage />} />
-        </Routes>
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_SIGN}>
+          <Routes path="/*">
+            <Route path="/*" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            {/* <Route path="/*" element={<h1>ERROR 404 PAGE NOT FOUND</h1>} /> */}
+            <Route path="/admin/*" element={<AdminHomePage />} />
+          </Routes>
+        </GoogleOAuthProvider>
       </BrowserRouter>
     </>
   );
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -2,11 +2,38 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import { useGoogleLogin } from "@react-oauth/google";
+
 export default function LoginPage() {
   const [email, setEmail] = useState("Your Email");
   const [password, setPassword] = useState("");
 
-  console.log(import.meta.env.VITE_BACKEND_URL);
+  const googleLogin = useGoogleLogin({
+    onSuccess: (res) => {
+      console.log(res);
+      axios
+        .post("http://localhost:5000/api/users/google", {
+          token: res.access_token,
+        })
+        .then((res) => {
+          if (res.data.message == "User created") {
+            toast.success(
+              "your account is created. now you can login via google"
+            );
+          } else {
+            localStorage.setItem("token", res.data.token);
+            if (res.data.user.type == "admin") {
+              window.location.href = "/admin";
+              toast.success("Welcome Admin");
+            } else {
+              window.location.href = "/";
+              toast.success("Welcome!");
+            }
+          }
+        });
+    },
+  });
+
   function login() {
     axios
       .post(import.meta.env.VITE_BACKEND_URL + "/api/users/login", {
@@ -52,6 +79,12 @@ export default function LoginPage() {
         />
         <button className="bg-yellow-400" onClick={login}>
           Login
+        </button>
+
+        <button
+          onClick={googleLogin}
+          className="bg-white mt-3 rounded px-3 py-1">
+          Login with Google
         </button>
       </div>
     </div>
